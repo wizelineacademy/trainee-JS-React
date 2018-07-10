5ff3c9332897b48e214306ca53c608386810eb06
Charly Castro <charlycastro@Charlys-MacBook-Pro.local>
2018-07-10 17:56:50 -0500

    state and controlled items

--- src/App.js
+++ src/App.js
@@ -1,16 +1,56 @@
 import React, { Component } from 'react';
 
 export default class App extends Component {
+  constructor (props) {
+    super(props);
+
+    this.state= {
+      text: '',
+      tasks: []
+    }
+  }
+
+  handleOnChange = e => {
+    this.setState({
+      text: e.target.value
+    })
+  }
+
+  addItem = () => {
+    let newItem = {
+      text: this.state.text,
+      key: Date.now()
+    };
+ 
+    this.setState((prevState) => {
+      return { 
+        tasks: prevState.tasks.concat(newItem),
+        text: ''
+      };
+    });
+  }
+
+  deleteItem = key => () => {
+    var filteredTasks = this.state.tasks.filter(function (task) {
+      return (task.key !== key);
+    });
+   
+    this.setState({
+      tasks: filteredTasks
+    });
+  }
+  
   render () {
     return (
       <div className="todoListMain">
         <div className="header">
-          <input placeholder="enter task" />
-          <button >add</button>
+          <input placeholder="enter task" value={ this.state.text } onChange={ this.handleOnChange } />
+          <button onClick={ this.addItem } >add</button>
           <ul className="theList">
-            <li>task 1</li>
-            <li>task 2</li>
-            <li>task 3</li>
+            {
+              this.state.tasks.map( task => 
+              <li onClick={ this.deleteItem(task.key) } key={ task.key } > {task.text} </li>)
+              }
           </ul>
         </div>
       </div>
