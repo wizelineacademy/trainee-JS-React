2123a385f83501101ab7fe7ae4d281c974cb3ffb
Charly Castro <charlycastro@Charlys-MacBook-Pro.local>
2018-07-10 17:08:22 -0500

    jsx and hellow world examples

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById('root'));
+ReactDOM.render(<h1>Hello World</h1>, document.getElementById('root'));
 registerServiceWorker();
