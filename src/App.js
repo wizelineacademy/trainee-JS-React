73a6c208f3c32c9946744769a1108ba3428ca9bd
Charly Castro <charlycastro@Charlys-MacBook-Pro.local>
2018-07-10 17:43:04 -0500

    app skeleton

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
-export default class App extends Component {
 
+export default class App extends Component {
   render () {
     return (
-      <div>
-        hello world
+      <div className="todoListMain">
+        <div className="header">
+          <input placeholder="enter task" />
+          <button >add</button>
+          <ul className="theList">
+            <li>task 1</li>
+            <li>task 2</li>
+            <li>task 3</li>
+          </ul>
+        </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<h1>Hello World</h1>, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
 registerServiceWorker();
